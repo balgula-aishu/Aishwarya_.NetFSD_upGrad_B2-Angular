f7734df342e148b4f717efba38cd8eb9b0bacfe4
balgula-aishu <160384161+balgula-aishu@users.noreply.github.com>
2026-04-14 22:15:48 +0530

    Add files via upload

---
+++ HandsOn-week8-day4/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactManagement.DAL.Models;
+
+namespace ContactManagement.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ContactInfo> Contacts { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactInfo>()
+                .HasOne(c => c.Company)
+                .WithMany(c => c.Contacts)
+                .HasForeignKey(c => c.CompanyId);
+
+            modelBuilder.Entity<ContactInfo>()
+                .HasOne(c => c.Department)
+                .WithMany(d => d.Contacts)
+                .HasForeignKey(c => c.DepartmentId);
+        }
+    }
+}
